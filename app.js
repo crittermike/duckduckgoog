3451db06f2cf65554c5bfdccde82aca51fd3cf77
Mike Crittenden <mikecrittenden@gmail.com>
2012-07-17 15:24:58 -0400

    initial commit (real)

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    http = require('http');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+  var query = "";
+  if (req.query["q"]) {
+    query = req.query["q"];
+    if (query.match(/![A-Za-z0-9]+/)) {
+      res.redirect('https://duckduckgo.com?q=' + query);
+    } else {
+      res.redirect('https://encrypted.google.com/search?q=' + query);
+    }
+  }
+  res.render('index', { query: req.query["q"] });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
